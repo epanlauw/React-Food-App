2507956325fb1d372dbd58503ea329207cfeca2d
epanlauw <stefanus.lauwrendo@codify.id>
2021-08-07 16:14:30 +0700

    Bug fix and useRef

--- src/components/Meals/AvaiableMeals/AvailableMeals.js
+++ src/components/Meals/AvaiableMeals/AvailableMeals.js
@@ -1,4 +1,4 @@
-import classes from "./AvaiableMeals.module.css";
+import classes from "./AvailableMeals.module.css";
 import Card from "../../UI/Card/Card";
 import MealItem from "../MealItem/MealItem";
 
--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -3,7 +3,9 @@ import React from "react";
 const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
-  addItem: (item) => {},
+  addItem: (item) => {
+    console.log(item);
+  },
   removeItem: (id) => {},
 });
 
